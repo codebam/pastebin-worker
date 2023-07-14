f36f954432ad44dd73d04e051b74d38ce666d72a
Sean Behan <codebam@riseup.net>
2023-07-13 23:58:07 -0400

    echo the type of request

--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,6 @@ use worker::*;
 
 #[event(fetch)]
 async fn main(req: Request, env: Env, ctx: Context) -> Result<Response> {
-    Response::ok("Hello, World!")
+    let method = req.method();
+    Response::ok(method)
 }

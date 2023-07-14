b7cc6614ea08cf91543143a801e638eb592321b0
Sean Behan <codebam@riseup.net>
2023-07-14 13:49:41 -0400

    update

--- src/lib.rs
+++ src/lib.rs
@@ -14,19 +14,19 @@ async fn main(req: Request, env: Env, _ctx: Context) -> Result<Response> {
                 .text().await
                 .map_err(|e| console_log!("{}", e)).unwrap()
                 .unwrap_or_else(|| "404".to_string());
-            if req.path().as_str() == "/" {
-                return Response::from_html(_result)
-            }
-            if _result.as_str() == "404" {
-                return Response::error(_result, 404)
-            }
-            if req.path().as_str().trim_end_matches(".*").len() > 0 {
-                return Response::from_body(
-                    ResponseBody::Body(_result.as_str().as_bytes().to_vec())
-                )
-            }
             return match _result.as_str() {
-                &_ => Response::ok(_result)
+                "404" => Response::error(_result, 404),
+                &_ => {
+                    if req.path().as_str() == "/" {
+                        return Response::from_html(_result)
+                    }
+                    if req.path().as_str().trim_end_matches(".*").len() > 0 {
+                        return Response::from_body(
+                            ResponseBody::Body(_result.as_str().as_bytes().to_vec())
+                        )
+                    }
+                    Response::ok(_result)
+                } 
             }
         }
         "POST" | "PUT" => {

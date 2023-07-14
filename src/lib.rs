5114ddb2ba54d3e440129784479c7b9d83f710a1
Sean Behan <codebam@riseup.net>
2023-07-14 12:38:26 -0400

    add DELETE method

--- src/lib.rs
+++ src/lib.rs
@@ -7,17 +7,6 @@ async fn main(req: Request, env: Env, _ctx: Context) -> Result<Response> {
     let mut req_mut = req.clone_mut().unwrap();
     let data = req_mut.text().await.unwrap();
     match method_str {
-        "POST" | "PUT" => {
-            if req.path().as_str() == "/" {
-                return Response::ok("cannot update /")
-            }
-            let _result = env.kv("rust_worker")
-                .map_err(|e| console_log!("{}", e)).unwrap()
-                .put(req.path().as_str(), data.clone())
-                .map_err(|e| console_log!("{}", e)).unwrap()
-                .execute().await;
-            Response::ok(data)
-        },
         "GET" => {
             let _result = env.kv("rust_worker")
                 .map_err(|e| console_log!("{}", e)).unwrap()
@@ -29,6 +18,23 @@ async fn main(req: Request, env: Env, _ctx: Context) -> Result<Response> {
                 "404" => Response::error(_result, 404),
                 &_ => Response::ok(_result)
             }
+        }
+        "POST" | "PUT" | "INSERT" => {
+            if req.path().as_str() == "/" {
+                return Response::ok("cannot update /")
+            }
+            let _result = env.kv("rust_worker")
+                .map_err(|e| console_log!("{}", e)).unwrap()
+                .put(req.path().as_str(), data.clone())
+                .map_err(|e| console_log!("{}", e)).unwrap()
+                .execute().await;
+            Response::ok(data)
+        },
+        "DELETE" => {
+            let _result = env.kv("rust_worker")
+                .map_err(|e| console_log!("{}", e)).unwrap()
+                .delete(req.path().as_str()).await;
+            Response::ok("deleted")
         }
         &_ => Response::ok(method)
     }

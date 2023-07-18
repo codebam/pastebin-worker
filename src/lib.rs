34ec17cf96f1ef66931fd0b10042f71fe6b8e1ce
Sean Behan <codebam@riseup.net>
2023-07-18 13:35:00 -0400

    allow deleting with get request

--- src/lib.rs
+++ src/lib.rs
@@ -12,35 +12,44 @@ fn console_error(e: Error) {
 }
 
 async fn post_put(_req: Request, ctx: RouteContext<()>) -> Result<Response> {
-            let mut req_mut = _req.clone_mut().map_err(|e| console_log!("{}", e)).unwrap();
-            let form_data = req_mut
-                .form_data().await.map_err(console_error).unwrap();
-            let form_entry = form_data.get("upload")
-                .unwrap_or_else(|| form_data.get("paste").unwrap());
-            let file = match form_entry {
-                FormEntry::Field(form_entry) => {
-                    File::new(form_entry.into_bytes(), "paste")
-                },
-                FormEntry::File(form_entry) => {
-                    form_entry
-                }
-            };
-            let filename = file.name();
-            let path = Path::new(filename.as_str()).file_prefix().unwrap_or_else(|| OsStr::new("")).to_str().unwrap_or_else(|| "");
-            let path_str = path;
-            if path_str == "/" {
-                return Response::ok("cannot update /")
-            }
-            let b64 = general_purpose::STANDARD.encode(&file.bytes().await.unwrap());
-            let _result = ctx.kv("rust_worker")
-                .map_err(console_error).unwrap()
-                .put(path_str, b64)
-                .map_err(|e| console_log!("{}", e)).unwrap()
-                .execute().await;
-            let url = _req.url().map_err(console_error).unwrap();
-            let redirect = url.to_string() + path_str;
-            let redirect_url = Url::parse(redirect.as_str()).unwrap();
-            Response::redirect(redirect_url)
+    let mut req_mut = _req.clone_mut().map_err(|e| console_log!("{}", e)).unwrap();
+    let form_data = req_mut
+        .form_data().await.map_err(console_error).unwrap();
+    let form_entry = form_data.get("upload")
+        .unwrap_or_else(|| form_data.get("paste").unwrap());
+    let file = match form_entry {
+        FormEntry::Field(form_entry) => {
+            File::new(form_entry.into_bytes(), "paste")
+        },
+        FormEntry::File(form_entry) => {
+            form_entry
+        }
+    };
+    let filename = file.name();
+    let path = Path::new(filename.as_str()).file_prefix().unwrap_or_else(|| OsStr::new("")).to_str().unwrap_or_else(|| "");
+    let path_str = path;
+    if path_str == "/" {
+        return Response::ok("cannot update /")
+    }
+    let b64 = general_purpose::STANDARD.encode(&file.bytes().await.unwrap());
+    let _result = ctx.kv("rust_worker")
+        .map_err(console_error).unwrap()
+        .put(path_str, b64)
+        .map_err(|e| console_log!("{}", e)).unwrap()
+        .execute().await;
+    let url = _req.url().map_err(console_error).unwrap();
+    let redirect = url.to_string() + path_str;
+    let redirect_url = Url::parse(redirect.as_str()).unwrap();
+    Response::redirect(redirect_url)
+}
+
+async fn delete(_req: Request, ctx: RouteContext<()>) -> Result<Response> {
+    let empty_string = String::new();
+    let file = ctx.param("file").unwrap_or_else(|| &empty_string).as_str();
+    let _result = ctx.kv("rust_worker")
+        .map_err(|e| console_log!("{}", e)).unwrap()
+        .delete(file).await;
+    Response::ok("deleted")
 }
 
 #[event(fetch)]
@@ -83,15 +92,8 @@ async fn main(req: Request, env: Env, _ctx: Context) -> Result<Response> {
         })
         .post_async("/", post_put)
         .put_async("/", post_put)
-        .delete_async("/:file", |_req, ctx| async move {
-            let empty_string = String::new();
-            let file = ctx.param("file").unwrap_or_else(|| &empty_string).as_str();
-            let _result = ctx.kv("rust_worker")
-                .map_err(|e| console_log!("{}", e)).unwrap()
-                .delete(file).await;
-            let url = _req.url().unwrap();
-            Response::redirect(url)
-        })
+        .delete_async("/:file", delete)
+        .get_async("/delete/:file", delete)
         .or_else_any_method_async("/", |req, _ctx| async move {
             Response::redirect(req.url().unwrap())
         })
